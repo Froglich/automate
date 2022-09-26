9758f2d2a3fe16913fefff9e067cd4a2e9a99745
Kim Lindgren <kim.lindgren@slu.se>
2022-09-26 09:13:46 +0200

    Implemented crontab support

--- README.md
+++ README.md
@@ -1,10 +1,53 @@
-# automate
+# Automate
 
-A utility for easily scheduling tasks in Windows (actually, it is cross-platform but cron is a better alternative on other platforms) written in Go. Gorma runs entirely in the background, with the exception of an icon in the tray (to show that it is active). 
+Automate is a basic and light-weight utility for easily scheduling tasks in Windows (actually, it is cross-platform but I see no reason why anyone wouldnt use a more feature rich and well documented cron daemon on e.g. Linux) written in Go. Automate runs entirely in the background, with the exception of an icon in the tray (to show that it is active). I am aware of the windows task scheduler, but it has never done anything for me other than contribute to my receeding hairline.
 
-**Currently, only the following functionality is available:**
-On a monthly, weekly, daily, hourly, and per minute basis, the application attempts to execute all files in the folders with corresponding names (month/week/day/hour/minute).
+## Folder based automation
+
+On a monthly, weekly, daily, hourly, and per minute basis, the application attempts to execute all files in the folders with corresponding names (month/week/day/hour/minute), empty folders are created in the working directory on launch if they do not exist.
 
 Commands run on startup, then on the hour for hourly tasks and at midnight for weekly and daily dasks.
 
-In the future, I may implement reading a crontab-file.
+## Crontab.txt
+
+Additionally, you may write jobs in a file name crontab.txt inside the working directory of automate (created on launch if it does not exist). Each line in crontab.txt should begin with 5 time tags separated with whitespace followed by the command to execute, like below:
+
+```
+.---------------- minute
+| .-------------- hour
+| | .------------ day of month
+| | | .---------- month
+| | | | .-------- day of week
+| | | | |
+* * * * * your-command arg1 arg2 arg3...
+```
+
+Day of week is given as a number between 1 and 7, where 1 is monday and 7 is sunday. The crontab.txt file is read once per minute. Examples of valid values in time tags are:
+
+| Value | Type            | Description                                                                                                 |
+|---    |---              |---                                                                                                          |
+| *     | Any             | Execute for any value in this category                                                                      |
+| 5     | Single value    | Execute when the value for that time-tag matches the value                                                  |
+| 1,5,9 | Multiple values | Execute when the value for that time-tag matches any value in the comma separated list                      |
+| 1-9   | Range           | Execute when the value for that time-tag falls inside the specified range                                   |
+| */5   | Fraction        | Execute when the value for that time-tag is divisible according to the given fraction (e.g every 5 minutes) |
+
+### Examples
+
+#### Run a job at 07:30
+
+```
+30 7 * * * command
+```
+
+#### Run a job at midning at the first day of every month
+
+```
+* 0 1 * * command
+```
+
+#### Run a job every 5 minutes
+
+```
+*/5 * * * * command
+```
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"encoding/csv"
+	"io"
 	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/getlantern/systray"
 )
+
+var pCrontab *regexp.Regexp = regexp.MustCompile(`^(\*(?:\/[0-9]+)?|[0-9]+(?:-[0-9]+|(?:,[0-9]+)+)?)\s+(\*(?:\/[0-9]+)?|[0-9]+(?:-[0-9]+|(?:,[0-9]+)+)?)\s+(\*(?:\/[0-9]+)?|[0-9]+(?:-[0-9]+|(?:,[0-9]+)+)?)\s+(\*(?:\/[0-9]+)?|[0-9]+(?:-[0-9]+|(?:,[0-9]+)+)?)\s+(\*(?:\/[0-9]+)?|[0-9]+(?:-[0-9]+|(?:,[0-9]+)+)?)\s+(.+)$`)
+var pFraction *regexp.Regexp = regexp.MustCompile(`^\*\/([0-9]+)$`)
+var pRange *regexp.Regexp = regexp.MustCompile(`^([0-9]+)-([0-9]+)$`)
+var pList *regexp.Regexp = regexp.MustCompile(`^[0-9]+(?:,[0-9]+)*$`)
 
 func main() {
 	// Should be called at the very beginning of main().
@@ -31,14 +41,14 @@ func listDir(path string) ([]fs.FileInfo, error) {
 	return files, nil
 }
 
-func runTask(task string) {
-	cmd := exec.Command(task)
-	log.Printf("executing [%s]", task)
+func runTask(task string, args ...string) {
+	cmd := exec.Command(task, args...)
+	log.Printf("executing [%s %v]", task, args)
 	err := cmd.Run()
 	if err != nil {
-		log.Printf("ERROR - [%s] finished with error: %v", task, err)
+		log.Printf("ERROR - [%s %v] finished with error: %v", task, args, err)
 	} else {
-		log.Printf("[%s] finished without errors.", task)
+		log.Printf("[%s %v] finished without errors.", task, args)
 	}
 }
 
@@ -70,12 +80,132 @@ func startTasker(taskdir string, interval time.Duration) {
 	}
 }
 
+type tabTask struct {
+	Minute    bool
+	Hour      bool
+	Day       bool
+	Month     bool
+	DOW       bool
+	Command   string
+	Arguments []string
+}
+
+func parseTabTaskTag(tag string, currentValue int) bool {
+	if tag == "*" {
+		return true
+	}
+
+	if m := pFraction.FindStringSubmatch(tag); len(m) == 2 {
+		f, _ := strconv.Atoi(m[1])
+
+		if f != 0 && currentValue%f == 0 {
+			return true
+		}
+
+		return false
+	}
+
+	if m := pRange.FindStringSubmatch(tag); len(m) == 3 {
+		min, _ := strconv.Atoi(m[1])
+		max, _ := strconv.Atoi(m[2])
+
+		if currentValue >= min && currentValue <= max {
+			return true
+		}
+
+		return false
+	}
+
+	if pList.MatchString(tag) {
+		alts := strings.Split(tag, ",")
+
+		for _, a := range alts {
+			v, _ := strconv.Atoi(a)
+
+			if v == currentValue {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func parseTabTask(forTime time.Time, qMinute, qHour, qDay, qMonth, qDOW, cmd string) *tabTask {
+	hour, minute, _ := forTime.Clock()
+	_, tMonth, day := forTime.Date()
+	month := int(tMonth)
+	dow := int(forTime.Weekday())
+
+	task := tabTask{
+		Minute: parseTabTaskTag(qMinute, minute),
+		Hour:   parseTabTaskTag(qHour, hour),
+		Day:    parseTabTaskTag(qDay, day),
+		Month:  parseTabTaskTag(qMonth, month),
+		DOW:    parseTabTaskTag(qDOW, dow),
+	}
+
+	r := csv.NewReader(strings.NewReader(cmd))
+	r.Comma = ' '
+	fields, err := r.Read()
+	if err != nil {
+		log.Printf("error while reading command [%s] in crontab.txt: '%v'", cmd, err)
+		return nil
+	}
+
+	task.Command = fields[0]
+	if len(fields) > 1 {
+		task.Arguments = fields[1:]
+	} else {
+		task.Arguments = make([]string, 0)
+	}
+
+	return &task
+}
+
+func tabTasks() {
+	for {
+		currentTime := time.Now().Truncate(time.Minute)
+
+		f, err := os.OpenFile("crontab.txt", os.O_CREATE, 0666)
+		if err != nil {
+			log.Printf("error opening crontab.txt: '%v'", err)
+			return
+		}
+		defer f.Close()
+
+		content, err := io.ReadAll(f)
+		if err != nil {
+			log.Printf("error reading crontab.txt: '%v'", err)
+		}
+
+		lines := strings.Split(string(content), "\n")
+		for _, l := range lines {
+			m := pCrontab.FindStringSubmatch(l)
+
+			if len(m) < 7 {
+				continue
+			}
+
+			task := parseTabTask(currentTime, m[1], m[2], m[3], m[4], m[5], m[6])
+			if task != nil && task.Minute && task.Hour && task.Day && task.Month && task.DOW {
+				go runTask(task.Command, task.Arguments...)
+			}
+		}
+
+		nextTime := currentTime.Add(time.Minute)
+		time.Sleep(time.Until(nextTime))
+	}
+}
+
 func setUpTasks() {
 	go startTasker("month", 7*24*30*time.Hour)
 	go startTasker("week", 7*24*time.Hour)
 	go startTasker("day", 24*time.Hour)
 	go startTasker("hour", time.Hour)
 	go startTasker("minute", time.Minute)
+
+	go tabTasks()
 
 	log.Printf("start-up complete")
 }
@@ -91,6 +221,7 @@ func onReady() {
 	} else {
 		log.SetOutput(f)
 	}
+	defer f.Close()
 
 	setUpTasks()
 
